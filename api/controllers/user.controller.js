425a4acb4f0b326d5f6548ee90594da392b613d6
anish98166 <anish98166@gmail.com>
2024-06-27 20:24:17 +0545

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,39 @@
+import User from "../models/user.model.js";
+import errorHandler from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
 export const test = (req, res) => {
   res.json({
     success: true,
     message: "Hello",
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
